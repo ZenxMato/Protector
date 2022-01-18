d16bad48016dcf8be640d7fa9524a837a8456a80
ZenxMato <97756244+ZenxMato@users.noreply.github.com>
2022-01-18 20:00:32 +0700

    Update userinfo.py

--- PrimeMega/modules/userinfo.py
+++ PrimeMega/modules/userinfo.py
@@ -458,7 +458,7 @@ def set_about_me(update: Update, context: CallbackContext):
 
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "❂ <b>Stats For <a href='https://t.me/PrimeMegaBot'>Prime Mega</a>:</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    stats = "❂ <b>Stats For <a href='https://t.me/ProtectorX_bot'>𝗣𝗿𝗼𝘁𝗲𝗰𝘁𝗼𝗿</a>:</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
     update.effective_message.reply_text(
         result,
